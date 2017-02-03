6f6b9867de2082a86ec63ef3a4dc5950f2a48bd8
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-04 00:05:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,6 +118,10 @@ app.get('/ui/:pageNumber', function (req, res) {
   res.send(createTemplate(pages[pageNumber]));
 });
 
+app.get('/ui/:filename', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', req.params.filename));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
