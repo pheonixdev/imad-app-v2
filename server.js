49bd5e4fba224484b06161e12915b978aebd3de7
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-03 13:26:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,31 +7,73 @@ app.use(morgan('combined'));
 
 var pages= {
     pageOne: {
-        title: 'First Page|Pheonixdev',
-        date: '2 Feb,2017',
-        content: `<p>
-        
-        
-        
-        `
+        title: 'Education|Pheonixdev',
+        date: '3 Feb,2017',
+        content: `<div>
+                    <h5 class="education_title">School education</h5>
+                    <p>
+                        1998-2001 : Infant Jesus School, N.Paravoor.
+                        2001-2002 : St.Thomas Central School, Mukkolakal.
+                        2003-2011 : Loyola School, Sreekaryam.
+                    </p>
+                    <h5 class="education_title">College education</h5>
+                    <p>
+                        2012-2016: Lourdes Matha College of Science and Technology, Kuttichal.
+                                   (Bachelor of Technology in Electronics and Communication)
+                    </p>
+                </div> `
     },
     pageTwo: {
-        title: 'Second Page|Pheonixdev',
-        date: '2 Feb,2017',
-        content: `
-        
-        
-        
-        `
+        title: 'Interests|Pheonixdev',
+        date: '3 Feb,2017',
+        content: `<div>
+                    <h5 class="interest_title">Football</h5>
+                    <p>
+                        <img src="/ui/soccerPlayers" alt="Football image" align="centre"/>
+                            Favorite Team: FC Barcelona
+                            <a href="https://www.fcbarcelona.com/">
+                            <img src="/ui/FCBBarcelona-Logo-cool-Wallpaper" alt="Barcelona image" align="centre"/>
+                            </a>
+                            Favorite Player: Lionel Messi
+                            <a href="https://en.wikipedia.org/wiki/Lionel_Messi">
+                            <img src="/ui/lionel-messi-barcelona" alt="Messi image" align="centre"/>
+                            </a>
+                    </p>
+                    <h5 class="interest_title">Coding</h5>
+                    <p>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                            <li>Android</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                        </ul>
+                    </p>
+                    <h5 class="interest_title">Gaming</h5>
+                    <p>
+                        <ul>
+                            <li><a href="https://en.wikipedia.org/wiki/Dota_2">DOTA</a></li>
+                            <li><a href="https://worldofwarcraft.com/en-us/">World of Warcraft<a></li>
+                            <li><a href="http://www2.ea.com/pc/fifa">Fifa</a></li>
+                        </ul>
+                    </p>
+                </div>`
     },
     pageThree: {
-        title: 'Third Page|Pheonixdev',
-        date: '2 Feb,2017',
+        title: 'Miscellaneous|Pheonixdev',
+        date: '3 Feb,2017',
         content: `
-        
-        
-        
-        `
+                <div>
+                    <h4>You can find me on:</h4>
+                    <p>
+                        <ul id="social_links">
+                            <li><a href="https://www.facebook.com/sabari.nath.52687">Facebook</a></li>
+                            <li><a href="https://www.instagram.com/sabri_nath/?hl=en">Instagram</a></li>
+                            <li><a href="linkedin.com/in/sabarinath-m-s-9389b3123">Linkedin</a></li>
+                        </ul>
+                    </p>
+                </div>         `
     }
 };
        
