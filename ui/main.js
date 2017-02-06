3b5d3fbcdd2bcdc6236426bc7416bfc0370b9cb1
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-06 23:23:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+//image workout
+var img = document.getElementbyId('profimage');
+var marginRight = 0;
+function moveLeft(){
+    marginRight = marginRight + 1;
+    img.style.marginRight = marginRight + "%";
+}
+img.onClick = function(){
+    var interval = setInterval(moveLeft,50);
+};
