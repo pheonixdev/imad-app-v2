f99b5a38dfcd13c2cfbca2aa1bd339714fb0130a
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-08 19:44:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,20 +5,20 @@ button.onclick = function() {
     
     //Create a request
     var request = new XMLHttpRequest();
-    
     //Capture the response and store in variable
-    if(request.readyState === XMLhttpRequest.DONE){
-        //Take some action
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLhttpRequest.DONE){
+            //Take some action
+            if(request.status === 200) {
+            var counter = request.responeText;
+            var spanbutton = document.getElementById('count');
+            spanbutton.innerHTML = counter.toString();
+            }
+        //Not done yet
         
-    }
-    //Not done yet
-    
-    if(request.status === 200) {
-        var counter = request.responeText;
-        var spanbutton = document.getElementById('count');
-        spanbutton.innerHTML = counter.toString();
-    }
+        }
+    };
     //Make a request 
-    
-    
+    request.open('GET','http://pheonixdev.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
