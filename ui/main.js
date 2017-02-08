dcb42fa9aa8d6d75fac47a7e05d98709b6231ec1
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-08 12:38:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++
@@ -1,8 +0,0 @@
-//counter code
-var button = document.getElementById('profButton');
-var clicked = 0;
-button.onclick = function(){
-    clicked = clicked + 1;
-    var spanbut = document.getElementById('count');
-    spanbut.innerHTML = clicked.toString();
-};
