a311b4a29f50940f213ba2713f368ca0638f653e
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-08 12:24:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,13 +2,7 @@
 var button = document.getElementById('profButton');
 var clicked = 0;
 button.onclick = function(){
-    //make a request to the counter end point
-    
-    //capture the response and store it in a variable
-    
-    //Render the variable in the correct span
     clicked = clicked + 1;
     var spanbut = document.getElementById('count');
     spanbut.innerHTML = clicked.toString();
-    
 };
