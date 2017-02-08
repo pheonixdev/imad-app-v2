26260c13faeb1bcea5e2e30862c3c1cc3d7a2c7d
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-08 12:38:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.get('/', function (req, res) {
 
 
 var counter = 0;
-app.get('/counter', function (req, res){
+app.get('/', function (req, res){
     counter = counter + 1;
-    res.send(counter.toString());
+    var spanvisit = counter.toString();
 });
 
 app.get('/style.css', function (req, res) {
