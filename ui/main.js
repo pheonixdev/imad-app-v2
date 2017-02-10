cf3a628f0724b1b01529f59cffaac0b462612a96
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-10 12:56:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,8 @@ submit.onclick = function() {
             //Take some action
             if(request.status === 200) {
             //Captures a list of names and render it as a list
-                var names = ['name1','name2','name3','name4', 'name5'];
+                var names = request.responseText;
+                names = JSON.parse(names);
                 var list = '';
                 for(var i = 0; i < names.length; i++){
                     list += '<li>' + names[i] + '<li>';
@@ -51,7 +52,7 @@ submit.onclick = function() {
     //Make a request 
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET','http://pheonixdev.imad.hasura-app.io/counter', true);
+    request.open('GET','http://pheonixdev.imad.hasura-app.io/submit-name?name=' +name, true);
     request.send(null);
     
 };
