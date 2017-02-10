4e387a803c524b7565a5cc3171d7592018452abe
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-10 11:56:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,20 @@ button.onclick = function() {
     request.open('GET','http://pheonixdev.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementByID('submit');
+submit.onclick = function() {
+    //Make a request to the server and send the name
+    
+    //Captures a list of names and render it as a list
+     var names = ['name1','name2','name3'];
+     var list = '';
+     for(var i = 0; i < names.length; i++){
+         list += '<li>' + names[i] + '<li>';
+     }
+     var ul = document.getElementById('namelist');
+     ul.innerHTML = list;
+};
