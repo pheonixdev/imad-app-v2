9ec4b9ddff7a5cb9b435f0588d985c71288dc0d1
Sabarinath M S <sabariloyola10@gmail.com>
2017-02-10 12:49:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,18 +24,34 @@ button.onclick = function() {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Make a request to the server and send the name
+     //Create a request
+    var request = new XMLHttpRequest();
+    //Capture the response and store in variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200) {
+            //Captures a list of names and render it as a list
+                var names = ['name1','name2','name3','name4', 'name5'];
+                var list = '';
+                for(var i = 0; i < names.length; i++){
+                    list += '<li>' + names[i] + '<li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+            //Not done yet
+                    
+        }
+    };
+    //Make a request 
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://pheonixdev.imad.hasura-app.io/counter', true);
+    request.send(null);
     
-    //Captures a list of names and render it as a list
-     var names = ['name1','name2','name3','name4', 'name5'];
-     var list = '';
-     for(var i = 0; i < names.length; i++){
-         list += '<li>' + names[i] + '<li>';
-     }
-     var ul = document.getElementById('namelist');
-     ul.innerHTML = list;
 };
