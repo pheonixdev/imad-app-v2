9367ca4bcc790e726982aee7a44fc9a5de5ad6ab
Sabarinath M S <sabariloyola10@gmail.com>
2017-03-03 11:45:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/counter', function( req, res) {
 });
 
 app.get('/test-db', function(req,res){
-    pool.query('SELECT * test', function(err,result){
+    pool.query('SELECT * FROM test', function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }
