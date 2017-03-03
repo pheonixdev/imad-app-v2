4a642cedf93f3642a8c5a63c294c5ba54a2183ab
Sabarinath M S <sabariloyola10@gmail.com>
2017-03-03 10:49:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'pheonixdev',
+    database:'pheonixdev',
+    host:'http://db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+};
+
+var pool = new Pool(config);
 
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/test-db', function(req,res){
+    pool.query('SELECT * test', function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    })
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -15,6 +36,7 @@ app.get('/counter', function( req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
+
 
 app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
