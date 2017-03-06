51d549d0aead0e1f2bfcb82d5c8c81a46b755191
Sabarinath M S <sabariloyola10@gmail.com>
2017-03-07 01:09:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,12 +33,18 @@ app.get('/test-db', function(req,res) {
 });
 
 app.get('/article/:articleName', function(req,res) {
-   pool.query('SELECT * FROM Article WHERE title = $1', [req.params.articleName], function(err) {
+   pool.query('SELECT * FROM Article WHERE title = $1', [req.params.articleName], function(err,result) {
        if(err) {
            res.status(500).send(err.toString());
        }
        else {
-           if (articleName.length)
+           if (result.rows.length === 0) {
+               res.status(404).send('Article Not Found');
+           }
+           else {
+               var articleData = result.rows[0];
+               res.send(createTemplate(articleData));
+           }
        }
    });
 });
