94aacbb5d107381ef8a773c9253cf379238f350d
Sabarinath M S <sabariloyola10@gmail.com>
2017-03-08 01:45:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,34 @@ app.post('/create-user', function(req, res) {
    });
 });
 
+app.post('/login', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" username = $1)', [username], function(err, result) {
+       if(err) {
+             res.status(500).send(err.toString());
+       } 
+       else {
+           if(result.rows.length === 0) {
+               res.status(403).send('username/password is invalid');
+           }
+           else {
+               //Match the password
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hash(password, salt); // Creating a hash based on submitted password and original salt
+               if(hashedPassword === dbString) {
+                   res.send('Credentials are correct');
+                }
+                   else {
+                         res.status(403).send('username/password is invalid');
+                   }
+           }
+            
+       }
+    });
+});
+
 var pool = new Pool(config);
 app.get('/test-db', function(req,res) {
     pool.query('SELECT * FROM test', function(err, result) {
