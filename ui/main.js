dc99a9d94eddccd6d5f35738d1d208eb045404ae
Sabarinath M S <sabariloyola10@gmail.com>
2017-03-24 01:10:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,31 @@
 //Submit username/password
 
-var submit = document.getElementById('submit_btn');
+var submit = document.getElementById('login_btn');
 submit.onclick = function() {
 
      //Create a request
     var request = new XMLHttpRequest();
-    //Capture the response and store in variable
-    request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
-            //Take some action
-            if(request.status === 200) {
-                console.log('User logged in');
-                alert('Logged In Successfully');
-            }
-            else if(request.status === 403) {
-                alert('Username/Password is incorrect');
-            }
-            else if(request.status === 500) {
-                alert('Something went wrong on the server');
-            }
-                    
-        }
-    };
-    //Make a request 
+   
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+    //Make the request 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
     console.log(username);
@@ -30,4 +33,46 @@ submit.onclick = function() {
     request.open('POST','http://pheonixdev.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username, password:password}));
+    submit.value = 'Logging in...';
 };
+
+var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
+          }
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
+    };
+}
+
+
+
+
+
+
+
+
+
