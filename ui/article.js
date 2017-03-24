c01fa8ece1fb1e1f586037160a3dd15292e673b4
Sabarinath M S <sabariloyola10@gmail.com>
2017-03-24 15:16:54 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -33,7 +33,7 @@ function loadCommentForm () {
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
-        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        request.open('POST', 'http://pheonixdev.imad.hasura-app.io/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
         submit.value = 'Submitting...';
@@ -52,7 +52,7 @@ function loadLogin () {
         }
     };
     
-    request.open('GET', '/check-login', true);
+    request.open('GET', 'http://pheonixdev.imad.hasura-app.io/check-login', true);
     request.send(null);
 }
 
@@ -89,7 +89,7 @@ function loadComments () {
         }
     };
     
-    request.open('GET', '/get-comments/' + currentArticleTitle, true);
+    request.open('GET', 'http://pheonixdev.imad.hasura-app.io/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
 
